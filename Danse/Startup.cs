c4277a9849b8c4e6a856c4bbd49bde574e51f7dd
alexandremachado <alexandre.machado@biig.fr>
2015-11-12 16:41:07 +0100

    Init

---
+++ Danse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Danse.Startup))]
+namespace Danse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
