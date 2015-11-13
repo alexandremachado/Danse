6fb4ff5088de9798fac10dd748b83231639e2c17
alexandremachado <alexandre.machado@biig.fr>
2015-11-13 13:48:37 +0100

    #A09 lesson by filter

--- Danse/Controllers/UsersApiController.cs
+++ Danse/Controllers/UsersApiController.cs
@@ -49,6 +49,22 @@ namespace Danse.Controllers
 
 
 
+        [HttpGet]
+        [Route("api/v1/lesson/filter/{start}/{end}/{zip}")]
+        public string getLessonFilter(string start, string end, string zip)
+        {
+            DateTime startDate = Convert.ToDateTime(start);
+            DateTime endDate = Convert.ToDateTime(end);
+
+            ILessonRepository user = new LessonRepository();
+            result = user.GetFilter(startDate, endDate, zip);
+
+            var json = new JavaScriptSerializer().Serialize(result);
+
+            return json;
+        }
+
+        
 
 
 
