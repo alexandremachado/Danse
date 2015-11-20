5b86382558a0afd3b9aabd720ab133f997b84134
pbouttier <pierre.bouttier@ynov.com>
2015-11-20 08:48:01 +0100

    #F14 Inclusion des bibliothèques: js et css, mises en place des controllers

---
+++ Danse/Controllers/EventController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Danse.Controllers
+{
+    public class EventController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult See()
+        {
+            return View();
+        }
+    }
+}
