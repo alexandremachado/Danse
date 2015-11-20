384e90cd938392cbf717e9cfcc7b0ca613d1f8d6
pbouttier <pierre.bouttier@ynov.com>
2015-11-20 11:58:29 +0100

    add jquery ui min

--- Danse/App_Start/BundleConfig.cs
+++ Danse/App_Start/BundleConfig.cs
@@ -10,7 +10,7 @@ namespace Danse
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-ui.js"));
+                        "~/Scripts/jquery-ui.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
