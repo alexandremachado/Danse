9f9899ced1bcd69dcac79b0f482d7fec14b83e24
pbouttier <pierre.bouttier@ynov.com>
2015-11-20 12:05:28 +0100

    fix jquery

--- Danse/App_Start/BundleConfig.cs
+++ Danse/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace Danse
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-ui.js", "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
