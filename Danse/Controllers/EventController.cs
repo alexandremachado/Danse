239d2d402686363f3d33c18ea2e4f718f2793324
pbouttier <pierre.bouttier@ynov.com>
2015-11-20 11:03:12 +0100

    add event controller

--- Danse/Controllers/EventController.cs
+++ Danse/Controllers/EventController.cs
@@ -6,20 +6,24 @@ using System.Web.Mvc;
 
 namespace Danse.Controllers
 {
-    public class EventController : Controller
+    public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult List()
+        public ActionResult About()
         {
+            ViewBag.Message = "Your application description page.";
+
             return View();
         }
 
-        public ActionResult See()
+        public ActionResult Contact()
         {
+            ViewBag.Message = "Your contact page.";
+
             return View();
         }
     }
